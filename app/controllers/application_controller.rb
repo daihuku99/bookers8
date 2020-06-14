ac225c0f22473b986fb7e8fe4a0c8fef2d7ebb31
daihuku99 <dai9huku@gmail.com>
2020-06-14 22:15:25 +0900

    update application_controller.rb/template.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters,if: :devise_controller?
+	before_action :request_path
 
 	def after_sign_in_path_for(resource)
 		user_path(current_user)
@@ -9,6 +10,14 @@ class ApplicationController < ActionController::Base
 	def after_sign_out_path_for(resource)
 		'/'
 	end
+
+	def request_path
+		@path = controller_path + '#' + action_name
+		def @path.is(*str)
+			str.map{|s| self.include?(s)}.include?(true)
+		end
+	end
+
 	private
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
